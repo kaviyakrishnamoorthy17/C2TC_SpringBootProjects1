6b60995bf6d8d10ae1a24e2560f771c7c397712c
Lenovo <Lenovo@DESKTOP-TCIFSAN>
2025-08-15 23:35:22 +0530

    Initial Commit

---
+++ course_management_module/src/test/java/com/tns/course/CourseManagementModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
